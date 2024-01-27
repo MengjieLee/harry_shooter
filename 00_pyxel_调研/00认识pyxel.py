836577f72a28df02b6dc8a93333ef3d3fe8897d6
harry <harry_zuo@hotmail.com>
2024-01-27 17:00:23 +0800

    feat(infra): POC

---
+++ 00_pyxel_调研/00认识pyxel.py
@@ -0,0 +1,13 @@
+import pyxel
+
+pyxel.init(160, 120)
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 1)
+
+pyxel.run(update, draw) # 回调函数 update, draw
